f0d1810050d730571473aba0f74f3c72b92c7f2a
raim0nda <50843861+raim0nda@users.noreply.github.com>
2020-05-18 09:25:52 +0100

    taxi-program.py

---
+++ taxi.py
@@ -0,0 +1,25 @@
+# Agree picking time and arrival time also distance and rate
+
+# Ask for payment method
+
+# Send the confirmation with all details listed above
+
+# Job to be sent out to nearby taxies in the road, who their system is on
+
+# Another text message sent to the customer to confirm the coming taxi (Brand and number of the car, car colour, driver name and arrival time)
+
+# When taxi arrives – the office makes a call to the number which were given and 5 MINUTES WAITING TIME
+
+# If you come within 5 minutes – MILES COUNTER will start
+
+# If you DON’T come within 5 minutes – driver go away
+
+# When the journey finished – PAYMENT (CARD or CASH) will be taken
+
+# Driver sent to close the job then Office close the call – and send another job to the driver if any
+
+# Office send another  txt to the number to leave a feedback if they wish
+
+# Rate different for night-day time, weekends , events or holidays.
+
+# Different night-day rates for entering car
